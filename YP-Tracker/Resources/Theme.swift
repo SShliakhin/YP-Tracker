a601816b2c128314404ecb4be79229a0f74ff703
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-04-10 08:39:19 +0300

    feat: add colors

--- YP-Tracker/Resources/Theme.swift
+++ YP-Tracker/Resources/Theme.swift
@@ -46,22 +46,97 @@ enum Theme {
 	}
 
 	// MARK: - Colors
+	private enum FlatColor {
+		enum Tracker {
+			static let red = UIColor(hex: 0xFD4C49)
+			static let lightOrange = UIColor(hex: 0xFF881E)
+			static let blue = UIColor(hex: 0x007BFA)
+			static let purple = UIColor(hex: 0x6E44FE)
+			static let green = UIColor(hex: 0x33CF69)
+			static let lightPurple = UIColor(hex: 0xE66DD4)
+			static let lightPink = UIColor(hex: 0xF9D4D4)
+			static let lightBlue = UIColor(hex: 0x34A7FE)
+			static let lightGreen = UIColor(hex: 0x46E69D)
+			static let indigo = UIColor(hex: 0x35347C)
+			static let orange = UIColor(hex: 0xFF674D)
+			static let pink = UIColor(hex: 0xFF99CC)
+			static let paleOrange = UIColor(hex: 0xF6C48B)
+			static let paleBlue = UIColor(hex: 0x7994F5)
+			static let brightPurple = UIColor(hex: 0x832CF1)
+			static let paleIndigo = UIColor(hex: 0xAD56DA)
+			static let palePurple = UIColor(hex: 0x8D72E6)
+			static let brightGreen = UIColor(hex: 0x2FD058)
+		}
+
+		enum DayNight {
+			static let black = UIColor(hex: 0x1A1B22)
+			static let white = UIColor(hex: 0xFFFFFF)
+			static let gray = UIColor(hex: 0xAEAFB4)
+			static let lightGray = UIColor(hex: 0xE6E8EB)
+			static let backgroundDay = UIColor(hex: 0xE6E8EB)
+			static let backgroundNight = UIColor(hex: 0x414141)
+			static let red = UIColor(hex: 0xF56B6C)
+			static let blue = UIColor(hex: 0x3772E7)
+		}
+	}
+
+	static func getTrackerColors() -> [UIColor] {
+		[
+			FlatColor.Tracker.red,
+			FlatColor.Tracker.lightOrange,
+			FlatColor.Tracker.blue,
+			FlatColor.Tracker.purple,
+			FlatColor.Tracker.green,
+			FlatColor.Tracker.lightPurple,
+			FlatColor.Tracker.lightPink,
+			FlatColor.Tracker.lightBlue,
+			FlatColor.Tracker.lightGreen,
+			FlatColor.Tracker.indigo,
+			FlatColor.Tracker.orange,
+			FlatColor.Tracker.pink,
+			FlatColor.Tracker.paleOrange,
+			FlatColor.Tracker.paleBlue,
+			FlatColor.Tracker.brightPurple,
+			FlatColor.Tracker.paleIndigo,
+			FlatColor.Tracker.palePurple,
+			FlatColor.Tracker.brightGreen
+		]
+	}
+
 	enum Color {
-		case ypRed
-		case ypBlue
-		case ypGreen
+		case main
+		case accent
+		case background
+		case attention
+		case white
+		case black
+		case gray
+		case lightGray
 	}
 
 	static func color(usage: Color) -> UIColor {
 		let customColor: UIColor
 
 		switch usage {
-		case .ypRed:
-			customColor = .systemRed
-		case .ypBlue:
-			customColor = .systemBlue
-		case .ypGreen:
-			customColor = .systemGreen
+		case .main:
+			customColor = UIColor.color(light: FlatColor.DayNight.black, dark: FlatColor.DayNight.white)
+		case .accent:
+			customColor = FlatColor.DayNight.blue
+		case .background:
+			customColor = UIColor.color(
+				light: FlatColor.DayNight.backgroundDay.withAlphaComponent(30.0),
+				dark: FlatColor.DayNight.backgroundNight.withAlphaComponent(85.0)
+			)
+		case .attention:
+			customColor = FlatColor.DayNight.red
+		case .white:
+			customColor = UIColor.color(light: FlatColor.DayNight.white, dark: FlatColor.DayNight.black)
+		case .black:
+			customColor = UIColor.color(light: FlatColor.DayNight.black, dark: FlatColor.DayNight.white)
+		case .gray:
+			customColor = FlatColor.DayNight.gray
+		case .lightGray:
+			customColor = FlatColor.DayNight.lightGray
 		}
 
 		return customColor
--- YP-Tracker/Resources/ViewController.swift
+++ YP-Tracker/Resources/ViewController.swift
@@ -71,37 +71,48 @@ import SwiftUI
 struct ViewProvider: PreviewProvider {
 	static var previews: some View {
 
+		let colors = Theme.getTrackerColors()
+
 		let label = UILabel()
 		label.font = UIFont.systemFont(ofSize: 34.0)
 		label.text = "Welcome"
+		label.textColor = colors[0]
+		label.backgroundColor = Theme.color(usage: .background)
 
 		let label34 = UILabel()
 		label34.font = Theme.font(style: .bold34)
 		label34.text = "Welcome"
+		label34.textColor = colors[1]
 
 		let label32 = UILabel()
 		label32.font = Theme.font(style: .bold32)
 		label32.text = "Welcome"
+		label32.textColor = colors[2]
 
 		let label19 = UILabel()
 		label19.font = Theme.font(style: .bold19)
 		label19.text = "Welcome"
+		label19.textColor = colors[3]
 
 		let label17 = UILabel()
 		label17.font = Theme.font(style: .regular17)
 		label17.text = "Welcome"
+		label17.textColor = colors[4]
 
 		let label16 = UILabel()
 		label16.font = Theme.font(style: .medium16)
 		label16.text = "Welcome"
+		label16.textColor = colors[5]
 
 		let label12 = UILabel()
 		label12.font = Theme.font(style: .medium12)
 		label12.text = "Welcome"
+		label12.textColor = colors[6]
 
 		let label10 = UILabel()
 		label10.font = Theme.font(style: .medium10)
 		label10.text = "Welcome"
+		label10.textColor = colors[7]
 
 		return Group {
 			VStack(spacing: 0) {
@@ -114,6 +125,7 @@ struct ViewProvider: PreviewProvider {
 				(label12 as UIView).preview().frame(height: 40)
 				(label10 as UIView).preview().frame(height: 40)
 			}
+			.preferredColorScheme(.dark)
 		}
 	}
 }
