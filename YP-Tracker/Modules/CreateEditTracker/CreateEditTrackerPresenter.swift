ab9fc5a6e31caad429e1ff7ce4951c22850c5f9f
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-05-24 01:20:58 +0300

    feat: add one more file - magic git

---
+++ YP-Tracker/Modules/CreateEditTracker/CreateEditTrackerPresenter.swift
@@ -0,0 +1,55 @@
+//
+//  ICreateEditTrackerPresenter.swift
+import Foundation
+
+protocol ICreateEditTrackerPresenter {
+	/// Подготавливает данные на редринг вьюконтроллеру
+	func present(data: CreateEditTrackerModels.Response)
+}
+
+final class CreateEditTrackerPresenter: ICreateEditTrackerPresenter {
+	weak var viewController: ICreateEditTrackerViewController?
+
+	func present(data: CreateEditTrackerModels.Response) {
+
+		let viewData: CreateEditTrackerModels.ViewModel
+
+		switch data {
+		case let .selectFilter(selectedFilter, allFilters):
+			let all = allFilters.count
+			var next = 1
+
+			let viewModel: [CreateEditTrackerModels.YPCellModel] = allFilters.map { filter in
+				let (hasDivider, outCorner) = getDecor(all: all, next: next)
+				let isSelected = filter == selectedFilter
+
+				next += 1
+				return CreateEditTrackerModels.YPCellModel(
+					type: .checkmarkType,
+					title: filter.description,
+					description: "",
+					hasDivider: hasDivider,
+					outCorner: outCorner,
+					isSelected: isSelected,
+					event: nil
+				)
+			}
+			viewData = .showFilters(viewModel)
+		}
+
+		viewController?.render(viewModel: viewData)
+	}
+}
+
+private extension CreateEditTrackerPresenter {
+	func getDecor(all: Int, next: Int) -> (hasDivider: Bool, outCorner: [CellCorner]) {
+		let hasDivider = all > next
+		var outCorner: [CellCorner] = []
+		if next == 1 {
+			outCorner = [.top]
+		} else if !hasDivider {
+			outCorner = [.bottom]
+		}
+		return (hasDivider, outCorner)
+	}
+}
