631eb3696b210c9f48f103199b94ee204f0af493
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-07-11 09:44:11 +0300

    fix: correct tests

--- YP-TrackerTests/YPTrackerTests.swift
+++ YP-TrackerTests/YPTrackerTests.swift
@@ -3,21 +3,16 @@ import SnapshotTesting
 @testable import YP_Tracker
 
 final class YPTrackerTests: XCTestCase {
-	private let mainVC: UIViewController = Di().makeTrackersModule().0
+	func testMainVC_shouldBeSuccess() {
+		let mainVC: UIViewController = Di().makeTrackersModule().0
 
-	func testDarkMainVC_shouldBeFail() {
-		// mainVC.overrideUserInterfaceStyle = .light // после создания образца раскоментировать
-		assertSnapshot(
-			matching: mainVC,
-			as: .image(traits: .init(userInterfaceStyle: .dark))
-		)
-	}
-
-	func testLightMainVC_shouldBeFail() {
-		// mainVC.overrideUserInterfaceStyle = .dark // после создания образца раскоментировать
 		assertSnapshot(
 			matching: mainVC,
 			as: .image(traits: .init(userInterfaceStyle: .light))
+		)
+		assertSnapshot(
+			matching: mainVC,
+			as: .image(traits: .init(userInterfaceStyle: .dark))
 		)
 	}
 }
