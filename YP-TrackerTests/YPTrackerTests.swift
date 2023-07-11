d3a0524f7f4b2c93b10e05a2e02ac580cb9f4199
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-07-11 08:21:20 +0300

    feat: add snapshot tests

---
+++ YP-TrackerTests/YPTrackerTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+import SnapshotTesting
+@testable import YP_Tracker
+
+final class YPTrackerTests: XCTestCase {
+	private let mainVC: UIViewController = Di().makeTrackersModule().0
+
+	func testDarkMainVC_shouldBeFail() {
+		// mainVC.overrideUserInterfaceStyle = .light // после создания образца расскоментировать
+		assertSnapshot(
+			matching: mainVC,
+			as: .image(traits: .init(userInterfaceStyle: .dark))
+		)
+	}
+
+	func testLightMainVC_shouldBeFail() {
+		// mainVC.overrideUserInterfaceStyle = .dark // после создания образца расскоментировать
+		assertSnapshot(
+			matching: mainVC,
+			as: .image(traits: .init(userInterfaceStyle: .light))
+		)
+	}
+}
