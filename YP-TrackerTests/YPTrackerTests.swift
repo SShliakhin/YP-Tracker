37e6c54ef95a23c4ea24b78b0be5188b71ed933e
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-07-16 19:08:56 +0300

     style: clean up

--- YP-TrackerTests/YPTrackerTests.swift
+++ YP-TrackerTests/YPTrackerTests.swift
@@ -3,15 +3,15 @@ import SnapshotTesting
 @testable import YP_Tracker
 
 final class YPTrackerTests: XCTestCase {
-	func testMainVC_shouldBeSuccess() {
-		let mainVC: UIViewController = Di().makeTrackersModule().0
+	func test_MainVC_shouldBeSuccess() {
+		let sut: UIViewController = Di().makeTrackersModule().0
 
 		assertSnapshot(
-			matching: mainVC,
+			matching: sut,
 			as: .image(traits: .init(userInterfaceStyle: .light))
 		)
 		assertSnapshot(
-			matching: mainVC,
+			matching: sut,
 			as: .image(traits: .init(userInterfaceStyle: .dark))
 		)
 	}
