037a4afeb2cbcf9bfadfacd444bb25c2f16cea61
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-07-18 12:26:18 +0300

    style: clean up

--- YP-Tracker/Library/Utils/Gradient/UIImage+Gradient.swift
+++ YP-Tracker/Library/Utils/Gradient/UIImage+Gradient.swift
@@ -7,14 +7,14 @@ extension UIImage {
 		direction: GradientDirection
 	) -> UIImage {
 		let gradientLayer = CAGradientLayer()
-		
+
 		gradientLayer.frame = bounds
 		gradientLayer.colors = colors.map(\.cgColor)
 		gradientLayer.startPoint = direction.startPoint
 		gradientLayer.endPoint = direction.endPoint
-		
+
 		let renderer = UIGraphicsImageRenderer(bounds: bounds)
-		
+
 		return renderer.image { ctx in
 			gradientLayer.render(in: ctx.cgContext)
 		}
